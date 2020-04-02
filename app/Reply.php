56428925d71f5664a385eddb41655c4f751d4ba6
Sam Mawhinney <sam@podcast.co>
2020-04-02 20:57:16 +0100

    a user can favorite replies

--- app/Reply.php
+++ app/Reply.php
@@ -10,8 +10,34 @@ class Reply extends Model
     /**
      * One to many inverse, a reply belongs to a owner(user)
      */
+
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+
+    /**
+     * A reply can be favorited.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    /**
+     * Favorite the current reply.
+     *
+     * @return Model
+     */
+    public function favorite()
+    {
+        $attributes = ['user_id' => auth()->id()];
+
+        if (! $this->favorites()->where($attributes)->exists()) {
+            return $this->favorites()->create($attributes);
+        }
     }
 }
